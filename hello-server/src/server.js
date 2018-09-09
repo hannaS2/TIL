2d10fc12a298c349d5b216883ea6215f18e1a961
j3552j@gmail.com <j3552j@gmail.com>
2018-09-09 17:16:24 +0900

    hello-server

---
+++ hello-server/src/server.js
@@ -0,0 +1,44 @@
+
+// Koa
+const Koa = require("koa");    // import
+const Router = require("koa-router");
+const logger = require("koa-logger");
+
+const app = new Koa();
+const router = new Router();
+
+// Router
+// 127.0.0.1:3000/users
+// 127.0.0.1:3000/users/hannah
+/*
+app.use(async ctx => {
+    ctx.body = "Hello, Koa"
+});
+*/
+
+router.get("/", async ctx => {
+    ctx.body = {
+        name: "Hello REST API Server",
+        version: "0.0.3",
+        links: {
+            user_url: "https://api.hannahi.xyz/users"
+        }
+    };
+});
+
+router.get("/users", async ctx => {
+    ctx.body = [
+        {
+            name: "Tom",
+            age: 42
+        }, {
+            name: "Alice",
+            age: 12
+        }
+    ]
+});
+
+app.use(logger());
+app.use(router.routes())
+    .use(router.allowedMethods());
+app.listen(3000);
