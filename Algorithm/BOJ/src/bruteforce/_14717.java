53a8051f2bf4e25603997c8b3fdc74aa7b0b8ef3
j3552j@gmail.com <j3552j@gmail.com>
2018-10-02 23:49:25 +0900

    이름 변경

--- Algorithm/BOJ/src/bruteforce/_14717.java
+++ Algorithm/BOJ/src/bruteforce/_14717.java
@@ -16,12 +16,13 @@ public class _14717 {
         sc.nextLine();
 
         boolean ttaeng = firstNum == secondNum;
-        if (ttaeng) {
+        if (ttaeng) {  // 땡인 경우
             win = cases - (10 - firstNum);
             percentage = df.format(win * 1.0 / cases);
         } else {
+            int count = 0;
             for (int i = 1; i <= 10; i++) {
-                for (int j = i + 1; j <= 10; j++) {
+                for (int j = i + 1; j <= 10; j++) {  // 끗인 경우
                     if ((i + j) % 10 < (firstNum + secondNum) % 10) {
                         if (firstNum == i || firstNum == j || secondNum == i || secondNum == j) {
                             win += 2;  // 2세트 이므로 만약 상대방이 a1을 가져간 경우는 (a2,x),(x,a2)
@@ -32,6 +33,7 @@ public class _14717 {
                 }
             }
 
+            System.out.println(win);
             percentage = df.format(win * 1.0 / cases);
         }
 
