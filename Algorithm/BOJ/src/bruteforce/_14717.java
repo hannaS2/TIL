cfd4205944e50d86369ba666b9ebf77762deb0bd
j3552j@gmail.com <j3552j@gmail.com>
2018-10-03 00:16:55 +0900

    14717 풀이 변경

--- Algorithm/BOJ/src/bruteforce/_14717.java
+++ Algorithm/BOJ/src/bruteforce/_14717.java
@@ -6,9 +6,9 @@ import java.util.Scanner;
 public class _14717 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int cases = 153;  // 18C2
+        int cases = 18*17/2;  // 18C2
         int win = 0;
-        String percentage = "0.000";
+        String percentage;
         DecimalFormat df = new DecimalFormat("0.000");
 
         int firstNum = sc.nextInt();
@@ -20,20 +20,18 @@ public class _14717 {
             win = cases - (10 - firstNum);
             percentage = df.format(win * 1.0 / cases);
         } else {
-            int count = 0;
             for (int i = 1; i <= 10; i++) {
                 for (int j = i + 1; j <= 10; j++) {  // 끗인 경우
                     if ((i + j) % 10 < (firstNum + secondNum) % 10) {
                         if (firstNum == i || firstNum == j || secondNum == i || secondNum == j) {
-                            win += 2;  // 2세트 이므로 만약 상대방이 a1을 가져간 경우는 (a2,x),(x,a2)
+                            win += 2;  // 2세트 이므로 만약 상대방이 a1(=a2)을 가져간 경우는 (a2,b1),(a2,b2)
                         } else {
-                            win += 4;  // (a1,x),(x,a1),(a2,x),(x,a2)
+                            win += 4;  // (a1,b1),(a1,b2),(a2,b1),(a2,b1)
                         }
                     }
                 }
             }
 
-            System.out.println(win);
             percentage = df.format(win * 1.0 / cases);
         }
 
