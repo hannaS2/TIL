e156442c2d34b21a023847dbd3fb15ecc52ba936
j3552j@gmail.com <j3552j@gmail.com>
2018-10-02 23:37:53 +0900

    brute force 문제 추가

--- Algorithm/BOJ/src/bruteforce/_14717.java
+++ Algorithm/BOJ/src/bruteforce/_14717.java
@@ -1,15 +1,40 @@
 package bruteforce;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class _14717 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        float percentage = 0;
+        int cases = 153;  // 18C2
+        int win = 0;
+        String percentage = "0.000";
+        DecimalFormat df = new DecimalFormat("0.000");
 
         int firstNum = sc.nextInt();
         int secondNum = sc.nextInt();
+        sc.nextLine();
 
-        
+        boolean ttaeng = firstNum == secondNum;
+        if (ttaeng) {
+            win = cases - (10 - firstNum);
+            percentage = df.format(win * 1.0 / cases);
+        } else {
+            for (int i = 1; i <= 10; i++) {
+                for (int j = i + 1; j <= 10; j++) {
+                    if ((i + j) % 10 < (firstNum + secondNum) % 10) {
+                        if (firstNum == i || firstNum == j || secondNum == i || secondNum == j) {
+                            win += 2;  // 2세트 이므로 만약 상대방이 a1을 가져간 경우는 (a2,x),(x,a2)
+                        } else {
+                            win += 4;  // (a1,x),(x,a1),(a2,x),(x,a2)
+                        }
+                    }
+                }
+            }
+
+            percentage = df.format(win * 1.0 / cases);
+        }
+
+        System.out.println(percentage);
     }
 }
