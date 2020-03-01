3441c823c2be5831e84af708ff62c200379e7009
hannaS2 <j3552j@gmail.com>
2020-03-02 00:43:44 +0900

    10. Default props

--- RN/RN101-class/App.js
+++ RN/RN101-class/App.js
@@ -3,6 +3,10 @@ import { Button, StyleSheet, Text, View } from 'react-native';
 import { thistle } from 'color-name';
 
 class CounterButton extends React.Component {
+  static defaultProps = {
+    counter: -1,
+  };
+
   constructor(props) {
     super(props);
 
@@ -32,7 +36,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <CounterButton counter={0}/>
-        <CounterButton counter={1}/>
+        <CounterButton count={1}/>
         <CounterButton counter={2}/>
         <CounterButton counter={3}/>
       </View>
