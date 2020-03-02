33d372760ea1afab8286b61faab98b896a3c0872
hannaS2 <j3552j@gmail.com>
2020-03-02 18:11:56 +0900

    13. Flexbox

--- RN/RN101-class/App.js
+++ RN/RN101-class/App.js
@@ -1,44 +1,13 @@
 import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
-import { thistle } from 'color-name';
-
-class CounterButton extends React.Component {
-  static defaultProps = {
-    counter: -1,
-  };
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      counter: props.counter,
-    };
-  }
-
-  clickHandler = () => {
-    this.setState({
-      counter: this.state.counter + 1,
-    });
-  };
-
-  render() {
-    return (
-      <View style={styles.buttonContainer}>
-        <Text>{this.state.counter}</Text>
-        <Button title={'click me!'} onPress={this.clickHandler}/>
-      </View>
-    );
-  }
-}
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <CounterButton counter={0}/>
-        <CounterButton count={1}/>
-        <CounterButton counter={2}/>
-        <CounterButton counter={3}/>
+        <Text>1</Text>
+        <Text>2</Text>
+        <Text>3</Text>
       </View>
     );
   }
@@ -50,8 +19,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  buttonContainer: {
-    width: '100%',
   },
 });
