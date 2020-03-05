60fc7277ce529c036540bfa4d02177705e7da1af
hannaS2 <j3552j@gmail.com>
2020-03-06 00:59:33 +0900

    03-2. Styling the Movies

--- React/movie-app/src/Movie.js
+++ React/movie-app/src/Movie.js
@@ -1,9 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
 // component가 state가 필요 없을 경우에는 class component가 될 필요 없다. => function component 사용
-function Movie({ id, year, title, summary, poster }) {
-  return <h4>{title}</h4>;
+function Movie({ year, title, summary, poster }) {
+  return (
+    <div class="movies_movie">
+      <img src={poster} alt={title} title={title} />
+      <div class="movie_data">
+        <h3 class="movie_title">{title}</h3>
+        <h5 class="movie_year">{year}</h5>
+        <p class="moview_summary">{summary}</p>
+      </div>
+    </div>
+  );
 }
 
 Movie.propTypes = {
