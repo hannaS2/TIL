7de00f4a5d6f69b1c75df5ae7b6f8f4051cd2a7a
hannaS2 <j3552j@gmail.com>
2020-03-06 02:32:10 +0900

    04-1. Building the Router

--- React/movie-app/src/App.js
+++ React/movie-app/src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
 
 function App() {
-  return <span>something</span>; // 여기에 router를 넣어서 Home.js를 보여줄지 About.js를 보여줄지 선택
+  // 라우터가 url을 보고 어느 컴포넌트를 불러올지 결정
+  // 리액트 라우터는 해당하는 컴포넌트를 모두 렌더링한다. 따라서 동시에 렌더링된다. => exact={true}를 사용하면 해당 url이 아니면 렌더링 하지 않는다.
+  return (
+    <HashRouter>
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+    </HashRouter>
+  );
 }
 
 export default App;
