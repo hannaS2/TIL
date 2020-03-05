8d5a1e7c7841b1599075129d89edd7ad4ca59f6a
hannaS2 <j3552j@gmail.com>
2020-03-06 01:38:50 +0900

    03-4. Styles

--- React/movie-app/src/Movie.js
+++ React/movie-app/src/Movie.js
@@ -5,12 +5,12 @@ import "./Movie.css";
 // component가 state가 필요 없을 경우에는 class component가 될 필요 없다. => function component 사용
 function Movie({ year, title, summary, poster, genres }) {
   return (
-    <div className="movies_movie">
+    <div className="movie">
       <img src={poster} alt={title} title={title} />
       <div className="movie_data">
         <h3 className="movie_title">{title}</h3>
         <h5 className="movie_year">{year}</h5>
-        <ul className="gneres">
+        <ul className="movie_genres">
           {genres.map((genre, index) => (
             <li key={index} className="genres_genre">
               {genre}
