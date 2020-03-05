f32f9499f11bc451c1a71c8961356e044f77ae55
hannaS2 <j3552j@gmail.com>
2020-03-06 03:08:18 +0900

    04-3. Sharing Props Between Routes

--- React/movie-app/src/components/Movie.js
+++ React/movie-app/src/components/Movie.js
@@ -1,25 +1,39 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
 // component가 state가 필요 없을 경우에는 class component가 될 필요 없다. => function component 사용
 function Movie({ year, title, summary, poster, genres }) {
   return (
-    <div className="movie">
-      <img src={poster} alt={title} title={title} />
-      <div className="movie_data">
-        <h3 className="movie_title">{title}</h3>
-        <h5 className="movie_year">{year}</h5>
-        <ul className="movie_genres">
-          {genres.map((genre, index) => (
-            <li key={index} className="genres_genre">
-              {genre}
-            </li>
-          ))}
-        </ul>
-        <p className="moview_summary">{summary.slice(0, 180)}...</p>
+    <Link
+      to={{
+        pathname: "/movie-detail",
+        state: {
+          year,
+          title,
+          summary,
+          poster,
+          genres
+        }
+      }}
+    >
+      <div className="movie">
+        <img src={poster} alt={title} title={title} />
+        <div className="movie_data">
+          <h3 className="movie_title">{title}</h3>
+          <h5 className="movie_year">{year}</h5>
+          <ul className="movie_genres">
+            {genres.map((genre, index) => (
+              <li key={index} className="genres_genre">
+                {genre}
+              </li>
+            ))}
+          </ul>
+          <p className="moview_summary">{summary.slice(0, 180)}...</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
--- React/movie-app/src/components/Navigation.js
+++ React/movie-app/src/components/Navigation.js
@@ -5,10 +5,17 @@ import "./Navigation.css";
 function Navigation() {
   return (
     <div className="nav">
-      {/* <div href="/">Home</div>  => 이렇게 HTML처럼 하면 페이지를 전부 리로딩한다. React의 Link를 사용하자 */}
-      {/* Link는 라우터 밖에서 사용할 수 없다. */}
       <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
+      {/* 라우터에 있는 모든 Route들은 props를 기본값으로 가진다. => 이를 사용해서 정보를 전달할 수 있다. */}
+      {/* <Link
+            to={{
+                pathname: "/about",
+                state: {
+                    fromNavigation: true
+                }
+                }}
+            > */}
+      <Link to="about">About</Link>
     </div>
   );
 }
