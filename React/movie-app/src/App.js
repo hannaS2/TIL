8bd4e0073dff4f6586b0748898e155acee5dae7e
hannaS2 <j3552j@gmail.com>
2020-03-05 19:41:37 +0900

    01-1. Reusable Components with JSX + Props

--- React/movie-app/src/App.js
+++ React/movie-app/src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
-import Potato from "./Potato";
+
+function Food({ favorite }) {
+  // props -> {props.favorite}을 ES6를 사용해서 속성값으로 받아올 수 있다.
+  return <h1>I like {favorite}</h1>;
+}
 
 function App() {
   return (
     <div>
       <h1>Hello!</h1>
-      <Potato />
+      <Food favorite="kimchi" />
+      <Food favorite="ramen" />
+      <Food favorite="samgiopsal" />
+      <Food favorite="chukumi" />
     </div>
   );
 }
