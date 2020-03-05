8f55806eee475f6257a07696d1247fb7c1facec1
hannaS2 <j3552j@gmail.com>
2020-03-05 21:16:08 +0900

    02-1. All you need to know about State

--- React/movie-app/src/App.js
+++ React/movie-app/src/App.js
@@ -7,11 +7,13 @@ class App extends React.Component {
   };
 
   add = () => {
-    console.log("add");
+    // this.state.count = 1;  => 작동하지 않는다. react는 render함수를 refresh하지 않기 때문 => setState 사용
+    // this.setState({ count: this.state.count + 1 });  => 이 방법(setState에서 state를 사용하는 벙법)은 별로 좋지 않은 방법
+    this.setState(current => ({ count: current.count + 1 })); // 이 방법이 외부의 상태에 의존하지 않는 가장 좋은 방법 (current는 react에서 지원해주는 현재 state를 얻는 방법)
   };
 
   minus = () => {
-    console.log("minus");
+    this.setState(current => ({ count: current.count - 1 }));
   };
 
   render() {
